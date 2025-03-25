fa2f9626d985240bd048ece5fab962b5fe1078fa
Yuli <yulia.yurchenko90@gmail.com>
2025-03-25 11:38:43 +0100

    add default functions to createContext

--- src/components/User-context/index.js
+++ src/components/User-context/index.js
@@ -1,3 +1,7 @@
 import { createContext } from "react"
 
-export const UserContext = createContext({ currentUser: null })
+export const UserContext = createContext({
+  currentUser: null,
+  logInUser: () => {},
+  logOutUser: () => {},
+})
