416792bcf827ef36e65f11e34f6e5e71b51c7832
Yuli <yulia.yurchenko90@gmail.com>
2025-11-02 20:05:40 +0100

    add lhci and circleci

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,7 +12,7 @@ export function RootLayout({ children }) {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
       <link
-        href="https://fonts.googleapis.com/css2?family=Kay+Pho+Du:wght@400;500;600;700&family=Lora:ital,wght@0,400..700;1,400..700&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Kay+Pho+Du:wght@400;700&family=Lora:wght@400;700&display=swap"
         rel="stylesheet" />
     </head>
     <body>
